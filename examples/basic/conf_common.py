83a6421f1998f244ad9ce2dd8e127ec8569cc18a
Marius Vikhammer <marius.vikhammer@espressif.com>
2021-04-29 12:28:41 +0800

    Add examples for esp-docs
    Add two simple examples, for more complex examples see use in esp-idf or other repos

---
+++ examples/basic/conf_common.py
@@ -0,0 +1,15 @@
+from esp_docs.conf_docs import *  # noqa: F403,F401
+
+languages = ['en', 'zh_CN']
+idf_targets = ['esp32']
+
+# link roles config
+github_repo = 'espressif/esp-idf'
+
+# context used by sphinx_idf_theme
+html_context['github_user'] = 'espressif'
+html_context['github_repo'] = 'esp-docs'
+
+# Extra options required by sphinx_idf_theme
+project_slug = 'esp-idf'
+versions_url = 'https://dl.espressif.com/dl/esp-idf/idf_versions.js'
