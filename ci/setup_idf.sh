13463bc78f80df5a574ee2e7e9a5500aff67ffed
Marius Vikhammer <marius.vikhammer@espressif.com>
2022-09-06 12:07:34 +0800

    CI: change jobs to only do shallow clones

---
+++ ci/setup_idf.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# $1: repo url
+# $2: commit id
+# $3: IDF_PATH
+# $4: toolchain target
+
+# sets up the repo incl submodules
+set -o errexit # Exit if command failed.
+
+if [ -z $2 ] || [ -z $1 ]  || [ -z $3 ] || [ -z $4 ] ; then
+    echo "Mandatory variables undefined"
+    exit 1;
+fi;
+
+# sets up the repo incl submodules with specified version as $2
+echo "Checking out repo $1 with commit $2"
+mkdir $3 && cd $3
+git init
+git remote add origin $1
+git fetch --depth 1 origin $2
+git checkout FETCH_HEAD
+
+git submodule init
+git submodule update --depth 1
+
+# Setup IDF env
+tools/idf_tools.py --non-interactive install --targets $4
+tools/idf_tools.py install-python-env
